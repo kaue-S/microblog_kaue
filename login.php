c2065bc81e938706a29f64d6598c4f205e7d64b2
Kauê Evangelista <kauesilva38@hotmail.com>
2023-10-17 10:22:29 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,17 @@
 <?php 
+use Microblog\ControleDeAcesso;
+use Microblog\Usuario;
 require_once "inc/cabecalho.php";
+
+//Programação das mensagens de feedback (campos obrigatórios, dados incorretos, saiu do sistema).
+
+if( isset($_GET['campos_obrigatorios'])){
+	$feedback = "Você deve logar primeiro!";
+
+}
+
+
+
 ?>
 
 
@@ -9,9 +21,11 @@ require_once "inc/cabecalho.php";
 
         <form action="" method="post" id="form-login" name="form-login" class="mx-auto w-50">
 
-                
-				<p class="my-2 alert alert-warning text-center"></p>
-
+				<?php if( isset($feedback) ){ ?>
+				<p class="my-2 alert alert-warning text-center">
+					<?=$feedback?>
+				</p>
+				<?php } ?>
 				<div class="mb-3">
 					<label for="email" class="form-label">E-mail:</label>
 					<input class="form-control" type="email" id="email" name="email">
@@ -24,6 +38,18 @@ require_once "inc/cabecalho.php";
 				<button class="btn btn-primary btn-lg" name="entrar" type="submit">Entrar</button>
 
 			</form>
+
+	<?php
+		if(isset($_POST['entrar'])){
+			//verificar se os campos foram preenchidos
+			if(empty($_POST['email']) || empty($_POST['senha'])){
+				header("location:login.php?campos_obrigatorios");
+			} else {
+				echo "ok, você pode logar...";
+			}
+		}
+	?>
+
     </div>
     
     
