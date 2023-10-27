328d336293e427e7199bf676bc079ba20c8cee56
Kauê Evangelista <kauesilva38@hotmail.com>
2023-10-27 10:53:56 -0300

    comit

--- src/Noticia.php
+++ src/Noticia.php
@@ -255,6 +255,22 @@
 
 
 
+        public function busca():array{
+                $sql = " SELECT id, titulo, data, resumo FROM noticias WHERE titulo LIKE :termo OR resumo LIKE :termo OR texto LIKE :termo ORDER BY data DESC";
+
+                try {
+                        $consulta = $this->conexao->prepare($sql);
+                        $consulta->bindValue(":termo", "%".$this->getTermo()."%", PDO::PARAM_STR);
+                        $consulta->execute();
+                        $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+                } catch (Exception $erro) {
+                        die("Erro ao buscar notícias: ".$erro->getMessage());
+                }
+
+                return $resultado;
+        }
+
+
 
 
 
